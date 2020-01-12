1ae7185315ee149f0234f538f16de1185fd819cd
denisg28 <53035874+denisg28@users.noreply.github.com>
2020-01-12 17:09:25 +0200

    Deleted code!!!

--- www.chtravel.zzz.com.ua/page/about.php
+++
@@ -1,9 +0,0 @@
-<? head('Про нас'); ?>
-<div class="box clearfix">
-	<h2>Про нас</h2>
-	<?
-		$about = mysqli_fetch_assoc(mysqli_query($CONNECT, "SELECT `about` FROM `other`;"));
-		echo $about['about'];
-	?>
-</div>
-<? foot(); ?>
